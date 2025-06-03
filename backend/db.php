22ee331bf05842b7f2ff639abf2476b9ef2126c3
IonutSaft <ionut.saft14@gmail.com>
2025-06-04 00:28:49 +0300

    Update: added responsiveness

--- backend/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost", "root", "", "mygamelist");
-if ($mysqli->connect_errno) {
-  echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-
-$mysqli->set_charset("utf8mb4");
-
-?>
